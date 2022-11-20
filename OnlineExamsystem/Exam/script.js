b01006d96b3cc985d301340c3149617716d87fd9
SoHailaNekHat <sohaila.wb@gmail.com>
2022-11-20 06:29:40 +0430

    javaScript file added

---
+++ OnlineExamsystem/Exam/script.js
@@ -0,0 +1,12 @@
+function hoverr(){
+  document.getElementById("first-row-a").style.boxShadow="1px 1px 30px purple";
+ 
+  // var object=document.getElementsByClassName("first-row-1","first-row-2");
+  // for (var i in object) {
+  //  object=object[i];
+  // object[i].getElementsByClassName("first-row-1","first-row-2","first-row-3","first-row-").style.boxShadow="
+  // }
+}
+function unhover(){
+  document.getElementById("first-row-a").style.boxShadow="10px 10px 10px rgba(0,0,0,0.15)";
+}
