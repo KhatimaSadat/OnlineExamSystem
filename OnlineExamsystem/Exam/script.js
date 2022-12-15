0c0ee1b0016efa9efcf5020f4479fdf7b379140a
SoHailaNekHat <sohaila.wb@gmail.com>
2022-12-15 12:51:36 +0430

    header of the Exam is responsive now

--- OnlineExamsystem/Exam/script.js
+++ OnlineExamsystem/Exam/script.js
@@ -1,3 +1,23 @@
+// for navigation bar and side bar javascript code
+const humbarger = document.getElementsByClassName("humbarger")[0];
+const list = document.getElementById('right');
+
+const profile = document.getElementsByClassName("profile")[0];
+const aside = document.getElementById('left2');
+humbarger.addEventListener('click' , ()=>{
+    list.classList.toggle('active');
+    aside.classList.toggle('active');
+    
+})
+
+profile.addEventListener('click', ()=>{
+    alert("clicked");
+//    aside.classList.toggle('active');
+})
+
+
+
+
 function hoverimg1(){
      document.getElementById("image-1").style.width="35px"
 }
